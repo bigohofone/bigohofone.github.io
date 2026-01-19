dbf3ec02cd145c7cc7a62f11d4c202d57c921a70
bigohofone <bigohofone@gmail.com>
2026-01-19 21:03:57 +0900

    landing social to sticky

--- src/data/profile.js
+++ src/data/profile.js
@@ -3,5 +3,5 @@ export const profile = {
     name: "Wonjun Oh",
     pronouns: "he/him",
     image: "/assets/img/profile.jpeg",
-    shortBio: "B.S. Student at Korea University."
+    shortBio: "B.S. Student, Korea University"
 };
